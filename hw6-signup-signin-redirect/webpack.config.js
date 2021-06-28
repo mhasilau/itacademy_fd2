164acd1324feafef768a9a6e92ac2c10afa857cb
”Mikhail <mikhail.hasilau@gmail.com>
2021-06-28 13:49:18 +0300

    main page

--- hw6-signup-signin-redirect/webpack.config.js
+++ hw6-signup-signin-redirect/webpack.config.js
@@ -11,15 +11,15 @@ module.exports = {
         new HTMLWebpackPlugin({
             filename: 'index.html',
             template: './src/index.html'
-        }),
-        new HTMLWebpackPlugin({
-            filename: 'sign-in.html',
-            template: './src/templates/sign-in/sign-in.html'
-        }),
-        new HTMLWebpackPlugin({
-            filename: 'sign-up.html',
-            template: './src/templates/sign-up/sign-up.html'
         })
+        // new HTMLWebpackPlugin({
+        //     filename: 'sign-in.html',
+        //     template: './src//components/sign-in/sign-in.html'
+        // }),
+        // new HTMLWebpackPlugin({
+        //     filename: 'sign-up.html',
+        //     template: './src/components/sign-up/sign-up.html'
+        // })
     ],
     module: {
         rules: [
