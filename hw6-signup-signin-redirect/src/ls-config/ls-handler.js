f9e0155672fc829cc0aee42bcfb3d9ee581e7dc3
”Mikhail <mikhail.hasilau@gmail.com>
2021-06-29 15:59:41 +0300

    not work sign up

--- hw6-signup-signin-redirect/src/ls-config/ls-handler.js
+++ hw6-signup-signin-redirect/src/ls-config/ls-handler.js
@@ -1,6 +1,6 @@
 export const getToken = () => localStorage.getItem('token');
 export const setToken = token => localStorage.setItem('token', token);
 export const removeToken = () => localStorage.removeItem('token');
-export const getLogin = () => localStorage.getItem('nickname');
-export const setLogin = nickname => localStorage.setItem('nickname', nickname);
-export const removeLogin = () => localStorage.removeItem('nickname');
+export const getLogin = () => localStorage.getItem('email');
+export const setLogin = email => localStorage.setItem('email', email);
+export const removeLogin = () => localStorage.removeItem('email');
