49f6d42ee570bfa041fa7c113a12cbcd6af59b85
mhasilau <mikhail.hasilau@gmail.com>
2021-06-29 20:50:33 +0300

    111

--- big-project/src/index.js
+++ big-project/src/index.js
@@ -1,15 +1,4 @@
-import { initApi, 
-    createUser, 
-    getUsers, 
-    createTodos, 
-    getTodos, 
-    updateUser, 
-    removeUser, 
-    createPost,
-    getPosts
-} from './api/api-handlers';
 import {renderPosts, postFormrender} from './dom-handlers/posts-renderer';
-
 import { routes, paths } from './shared/constants/routes';
 import { signInhandler } from './components/sign-in/sign-in';
 import { getToken } from './shared/ls-service';
@@ -18,47 +7,7 @@ import { signUpHandler } from './components/sign-up/sign-up';
 
 import './styles/styles.scss';
 
-
-
-// const user = {
-//     username: 'jane',
-//     age: 29,
-//     createionDate: moment().format('LLL')
-// }
-
-
-// const todo ={
-//     title: '3rd title',
-//     createionDate: moment().format('LLL')
-// }
-
-
-// // createUser(user);
-
-// getUsers();
-
-// // createTodos(todo);
-// getTodos();
-// // removeUser();
-// updateUser();
-
-
-
-
-
-
-
-
-
-// getPosts();
-// renderPosts();
-
-
 window.onload = () => {
-    // if (window.location.pathname === '/') {
-    //     window.location.href = 'sign-in.html';
-    // }
-
     const pathname = Object.values(paths).find( path => path === window.location.pathname);
 
     switch (pathname) {
@@ -72,7 +21,6 @@ window.onload = () => {
                 postFormrender();
                 logoutBtnHandler();
             }
-
             
             break;
         case paths.sign_in:
