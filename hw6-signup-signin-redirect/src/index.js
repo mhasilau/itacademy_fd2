a815d395ee88539c55fa4305f0ba6ef878bfb7e0
mhasilau <mikhail.hasilau@gmail.com>
2021-06-29 20:57:02 +0300

    new2

---
+++ hw6-signup-signin-redirect/src/index.js
@@ -0,0 +1,24 @@
+import { routes, paths } from './shared/constants/routes';
+import { signInHandler } from './components/sign-in/sign-in';
+import { getToken } from './ls-config/ls-handler';
+
+import './styles/style.scss';
+
+window.onload = () => {
+    const pathname = Object.values(paths).find( path => path === window.location.pathname);
+
+    switch (pathname) {
+        case paths.home:
+            const token = getToken();
+
+            if (!token) {
+                window.location.href = routes.sign_in;
+            }
+            break;
+        case paths.sign_in:
+            signInHandler();
+        default:
+            break;
+    }
+    
+}
