6bc0137c4cee7248786468bec60fc45d5cd23df8
mhasilau <mikhail.hasilau@gmail.com>
2021-07-09 17:15:54 +0300

    task 1 and 2

--- hw6-signup-signin-redirect/src/ls-config/ls-handler.js
+++
@@ -1,6 +0,0 @@
-export const getToken = () => localStorage.getItem('token');
-export const setToken = token => localStorage.setItem('token', token);
-export const removeToken = () => localStorage.removeItem('token');
-export const getLogin = () => localStorage.getItem('email');
-export const setLogin = email => localStorage.setItem('email', email);
-export const removeLogin = () => localStorage.removeItem('email');
